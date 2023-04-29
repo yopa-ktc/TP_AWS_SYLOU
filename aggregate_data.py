75058c5dd16fa796f32a19bd0b944313d69e805f
Diouf <sylvaindiouf18@gmail.com>
2023-04-30 00:26:37 +0200

    Modications
    Voici les modifs

--- aggregate_data.py
+++ aggregate_data.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 
 # Charger les données
-data1 = pd.read_csv('./samples/messages.csv')
-data2 = pd.read_csv('./samples/users.csv')
+data1 = pd.read_csv('C:/Users/sylva/OneDrive/Bureau/API_Docker_Hetic_2023/Docker_Api_Tp_Final/TP_AWS_SYLOU/samples/messages.csv')
+data2 = pd.read_csv('C:/Users/sylva/OneDrive/Bureau/API_Docker_Hetic_2023/Docker_Api_Tp_Final/TP_AWS_SYLOU/samples/users.csv')
 
 # Renommer la colonne 'author_id' pour pouvoir fusionner les dataframes
 data1 = data1.rename(columns={'author_id': 'user_id'})
@@ -14,17 +14,27 @@ pipeline_result = pd.merge(data1, data2, on='user_id')
 
 # Créer une liste des identifiants d'utilisateurs uniques
 unique_users = pipeline_result['user_id'].unique()
+unique_users2 = pipeline_result['first_name'].unique()
+
+# Fonction de formatage pour afficher le first_name sans parenthèses
+def format_func(user_id):
+    first_name = unique_users2[unique_users==user_id][0]
+    return first_name
 
 # Sélectionner l'utilisateur à afficher
 selected_user = st.selectbox('Sélectionner un utilisateur', unique_users)
 
-# Filtrer les messages de l'utilisateur sélectionné
-user_messages = pipeline_result[pipeline_result['user_id'] == selected_user]['content']
+# Récupérer le first_name et le content correspondant à l'utilisateur sélectionné
+selected_first_name = pipeline_result.loc[pipeline_result['user_id']==selected_user, 'first_name'].iloc[0]
+selected_content = pipeline_result.loc[pipeline_result['user_id']==selected_user, 'content'].tolist()
 
-# Afficher les messages
-st.write('Messages de l\'utilisateur sélectionné :')
-for message in user_messages:
+# Afficher le first_name et le content correspondant à l'utilisateur sélectionné
+st.write(f"Prénom: {selected_first_name}")
+
+st.write("Messages de l'utilisateur sélectionné :")
+for message in selected_content:
     st.write('- ' + message)
+
     
-# Sauvegarder le dataframe au format CSV
-pipeline_result[['user_id', 'content']].to_csv('pipeline_result.csv', index=False)
+# # Sauvegarder le dataframe au format CSV  
+# pipeline_result[['user_id', 'first_name', 'content']].to_csv('pipeline_result1.csv', index=False)
--- feed_database.py
+++ feed_database.py
@@ -1,26 +1,26 @@
+import streamlit as st
 import pandas as pd
-import mysql.connector
+import base64
 
-df = pd.read_csv("tp_final_data_pipeline/samples/users.csv")
+# Fonction pour télécharger un fichier CSV
+def download_csv(df, file_name):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{file_name}">Télécharger {file_name}</a>'
+    return href
 
-def stockageJsonSQL(data):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="leaderboard"
-    )
-    cursor = mydb.cursor()
-    #Remplissage de la langue french
-    for row in data["first_name"]:
-        sql = "INSERT INTO users (first_name) VALUES (%s)"
-        val = (row,)
-        cursor.execute(sql, val)
-    #Validation finales des insertions
-    mydb.commit()
-    print(cursor.rowcount, "enregistrement inséré.")
-    #Fermeture de la connexion SQL
-    cursor.close()
-    mydb.close()
-    
-stockageJsonSQL(df)
+# Chargement des fichiers CSV
+data1 = pd.read_csv('')
+data2 = pd.read_csv('C:/Users/sylva/OneDrive/Bureau/API_Docker_Hetic_2023/Docker_Api_Tp_Final/TP_AWS_SYLOU/samples/users.csv')
+
+# Affichage des tableaux
+st.write("Tableau 1 :")
+st.write(data1)
+
+st.write("Tableau 2 :")
+st.write(data2)
+
+# Bouton de téléchargement
+if st.button('Télécharger les données'):
+    st.markdown(download_csv(data1, 'message.csv'), unsafe_allow_html=True)
+    st.markdown(download_csv(data2, 'user.csv'), unsafe_allow_html=True)
